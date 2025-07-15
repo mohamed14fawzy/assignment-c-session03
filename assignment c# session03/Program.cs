57a1e5a27fa98a0ad2c86b7ce2e5419ed668a325
mohamed14fawzy <mohamed14fawzyy@gmail.com>
2025-07-16 00:26:18 +0300

    Add project files.

---
+++ assignment c# session03/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace assignment_c__session03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region question one 
+            /*
+                        string input;
+                        int result;
+                        do
+                        {
+                            Console.WriteLine("please enter your number");
+                            input = Console.ReadLine();
+                        }
+                        while (!(int.TryParse(input, out result)));
+                        Console.WriteLine($"you entered {result}");
+
+
+                        */
+            #endregion
+
+
+            #region question 2
+            //convert method throws exception when entering invaalid conversion
+            #endregion
+
+
+            #region question 3
+            //float a = 1.1234567;
+            //float b = 2.1234567;
+            //Console.WriteLine(a + b);
+            #endregion
+
+            #region question 4
+            string word = "hello";
+            string substring = word.Substring(0, 2);
+            #endregion
+
+# reion question 5 
+
+#endregion
+        }
+    }
+}
