6d3e53cad63bfed47d2a1e8b94458f5b415d810f
mohamed14fawzy <mohamed14fawzyy@gmail.com>
2025-07-16 20:07:55 +0300

    assignment session 3

--- assignment c# session03/Program.cs
+++ assignment c# session03/Program.cs
@@ -38,6 +38,104 @@
             //#endregion
 
             #endregion
+
+            #region question 5
+            //int num1 = 10;
+            //int num2 = 50;
+            ////Console.WriteLine($"first number is { num1 } and second number is { num2 }");
+            //num1 = num2;
+            //Console.WriteLine($"first number is { num1 } and second number is { num2 }");
+            ////value of num1 is holding the same value as num1
+
+            #endregion
+
+            #region question6
+            //string Fname = "mohamed";
+            //string Sname = "fawzy";
+            //Console.WriteLine($"first name is {Fname} and second name is {Sname}");
+            //Fname = Sname;
+            //Console.WriteLine($"first name is {Fname} and second name is {Sname}");
+            #endregion
+
+            #region question 7
+            //Console.WriteLine("enter your first name");
+            //string fname = Console.ReadLine();
+            //Console.WriteLine("enter your second name");
+            //string sname = Console.ReadLine();
+
+            //string fullname = ($"full name is {fname} {sname}");
+            //Console.WriteLine(fullname);
+
+
+            #endregion
+
+            #region question 8
+            //Note : The formula for simple interest is Interest = (principal * rate * time ) /100.
+
+            //double principle = 12;
+            //double rate = 15.7777;
+            //double time = 1.772;
+
+            //double interest = (principle * rate * time) / 100;
+            //Console.WriteLine(interest);
+
+
+            #endregion
+
+            #region question 9
+            //Note :The formula for BMI is BMI = (Weight)/(Height*Height)
+
+            //float weight = 90;
+            //float height = 182;
+
+            //float BMI = weight / (height * height);
+            //Console.WriteLine(BMI);
+
+            #endregion
+
+
+            #region q10
+            /*that below 10 degrees is "Just Cold"
+            above 30 degrees is "Just Hot"
+            anything else is "Just Good"
+            */
+
+            //Console.WriteLine("enter the temprature ");
+            //int.TryParse(Console.ReadLine(), out int temp);
+            //string result = temp <10? "just cold" : temp > 30 ? "just hot " : "just good";
+            //Console.WriteLine(result);
+
+
+
+
+
+            #endregion
+
+            #region q11
+            //Console.Write("enter the day ");
+            //int.TryParse(Console.ReadLine(), out int day);
+
+            //Console.Write("enter the month ");
+            //int.TryParse(Console.ReadLine(), out int month);
+
+            //Console.Write("enter the year ");
+            //int.TryParse(Console.ReadLine(), out int year);
+
+            //Console.WriteLine($"{day},{month},{year}");
+            //Console.WriteLine($"{day}/{month}/{year}");
+            //Console.WriteLine($"{day}-{month}-{year}");
+            //#endregion
+            #endregion
+
+            #region q12
+
+            //Console.WriteLine("enter your number");
+            //int.TryParse(Console.ReadLine(), out int result);
+            //result  % 3 == 0 ? && result % 4 == 0 ? "yes" : "no";
+
+
+            #endregion
+
         }
     }
 }
